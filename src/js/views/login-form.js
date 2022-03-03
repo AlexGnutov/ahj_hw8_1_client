97715761383c9293b71d9f14c9978df0c5991354
AlexGnutov <81751916+AlexGnutov@users.noreply.github.com>
2022-03-03 11:43:59 +0300

    message removed

--- src/js/views/login-form.js
+++ src/js/views/login-form.js
@@ -7,7 +7,7 @@ export default class LoginForm {
       <form id="ch-form" class="ch-form">
         <h3>Enter username</h3>
         <input class="ch-input" name="username">
-        <div class="ch-server-message">test message</div>
+        <div class="ch-server-message"></div>
         <button class="ch-form-button">Continue</button>
       </form>
     `;
